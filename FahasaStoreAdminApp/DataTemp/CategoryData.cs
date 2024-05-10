2bb9b86e2dda22fd474fd3bcaaef90d77fe0b6c6
Trinh Le <khanhtrinh.phuyen@gmail.com>
2024-05-10 11:06:53 +0700

    CategoryController

---
+++ FahasaStoreAdminApp/DataTemp/CategoryData.cs
@@ -0,0 +1,31 @@
+﻿using FahasaStoreAPI.Models.FormModels;
+
+namespace FahasaStoreAdminApp.DataTemp
+{
+    public class CategoryData
+    {
+        public List<CategoryForm> Categorys { get; }
+
+        public CategoryData()
+        {
+            Categorys = [];
+            for (int i = 1; i < 13; i++)
+            {
+                string urlImage = "/image/MenuImage/lf" + i + ".png";
+                CategoryForm Category = new(i, "Category " + i, urlImage);
+                Categorys.Add(Category);
+            }
+        }
+
+        public CategoryForm? Category(int id)
+        {
+            var Category = Categorys.SingleOrDefault(e => e.CategoryId == id);
+            return Category;
+        }
+
+        public IEnumerable<CategoryForm> ListCategorys()
+        {
+            return Categorys;
+        }
+    }
+}
--- FahasaStoreAdminApp/Models/FormModels/CategoryForm.cs
+++ FahasaStoreAdminApp/Models/FormModels/CategoryForm.cs
@@ -2,6 +2,13 @@
 {
     public class CategoryForm
     {
+        public CategoryForm(int categoryId, string name, string imageUrl)
+        {
+            CategoryId = categoryId;
+            Name = name;
+            ImageUrl = imageUrl;
+        }
+
         public int CategoryId { get; set; }
         public string Name { get; set; } = null!;
         public string ImageUrl { get; set; } = null!;
