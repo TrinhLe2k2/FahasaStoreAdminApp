110cd4a41103a9c19ea15e6df000066d5ec366f2
Trinh Le <khanhtrinh.phuyen@gmail.com>
2024-05-11 09:38:54 +0700

    AuthorController

--- FahasaStoreAdminApp/Models/FormModels/AuthorForm.cs
+++ FahasaStoreAdminApp/Models/FormModels/AuthorForm.cs
@@ -2,6 +2,12 @@
 {
     public class AuthorForm
     {
+        public AuthorForm(int authorId, string name)
+        {
+            AuthorId = authorId;
+            Name = name;
+        }
+
         public int AuthorId { get; set; }
         public string Name { get; set; } = null!;
     }
